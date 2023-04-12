75a50a4e76855ab081058f66f2d7a35d6c1b0aed
Madjda-Rezig <rezigmadjda@yahoo.com>
2023-04-12 12:15:01 +0100

    film

--- src/Pages/Stream.jsx
+++ src/Pages/Stream.jsx
@@ -1,8 +1,11 @@
 import Footer from "../Component/Footer/Footer.jsx";
+import Player from "../Component/Player/Player.jsx";
+import Window from "../Component/window/Window.jsx";
 
 export default function Stream() {
   return (
     <div className="">
+      <Player />
       <Footer />
     </div>
   );
